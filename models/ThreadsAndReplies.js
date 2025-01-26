f0d95222530936f73c7440b1a09a0e390dc179d3
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-26 02:21:15 -0500

    add Thread and Reply mongoose models

---
+++ models/ThreadsAndReplies.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const ReplySchema = new mongoose.Schema({
+	text: {type: String, required: true},
+	created_on: {type: Date, default: Date.now},
+	reported: {type: Boolean, default: false},
+	delete_password: {type: String, required: true}
+})
+
+const ThreadSchema = new mongoose.Schema({
+	text: {type: String, required: true},
+	created_on: {type: Date, default: Date.now},
+	bumped_on: {type: Date, default: Date.now},
+	reported: {type: Boolean, default: false},
+	delete_password: {type: String, required: true},
+	replies: {type: [ReplySchema], default: []}
+})
+
+const Thread = mongoose.model('Thread', ThreadSchema)
+
+module.exports = Thread
