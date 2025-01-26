601c8e11946e19e086469456683c69bbd1bec22d
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-26 17:40:27 -0500

    add post route to "/api/threads/:board

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,26 @@
 'use strict';
+const express = require('express')
+const Thread = require('../models/ThreadsAndReplies')
+const mongoose = require('mongoose')
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board')
+	.post(async (req, res) => {
+		let { text, delete_password } = req.body;
+		const board = req.params.board;
+
+		let thread = await Thread.create({
+			board: board,
+			text: text,
+			delete_password: delete_password
+		});
+
+		return res.sendFile(process.cwd() + '/views/board.html')
+	})
+
+	
+		
     
   app.route('/api/replies/:board');
 
